f4a6151a6b9ba87f14791fda059cc235ac451388
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-12 22:25:26 +0200

    bottom TextView Scrollbar is invisible

--- app/src/main/java/com/example/beeradvisor/MainActivity.java
+++ app/src/main/java/com/example/beeradvisor/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.beeradvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view) {
+
+        TextView brands = findViewById(R.id.brands);
+        Spinner color = findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        brands.setText(beerType);
+
+
+
+
     }
 }
